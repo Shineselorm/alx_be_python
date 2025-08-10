51620652e755ca4bdbb14f84f374e07571f4dcc8
Shine Selorm <selomshine1@gmail.com>
2025-08-10 20:26:38 +0000

    Add Book class with constructor, destructor, and representation methods

---
+++ oop/book_class.py
@@ -0,0 +1,27 @@
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        """
+        Constructor to initialize the book instance.
+        """
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """
+        Destructor called when the object is deleted.
+        """
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        """
+        Informal string representation for users.
+        """
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """
+        Official string representation for developers.
+        Should return code-like representation to recreate the object.
+        """
+        return f"Book('{self.title}', '{self.author}', {self.year})"
